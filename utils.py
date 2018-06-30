7b6c882399f77ea784b91f5390551898282145c4
Krzysztof <joachimiak@mywhizzy.com>
2018-06-30 20:48:13 +0200

    NN

--- utils.py
+++ utils.py
@@ -4,6 +4,6 @@ def make_submission(ids, predictions):
     df = pd.concat([ids, pd.Series(predictions)], axis=1)
     return df.rename(columns={0: 'value'})
 
-from catboost import CatBoostRegressor, CatBoost
+from cb import CatBoostRegressor, CatBoost
 
 
