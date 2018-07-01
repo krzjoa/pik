735ba26c99d96e69aab41f820d9d4454f28ef195
Krzysztof Joachimiak <kjoa.92@gmail.com>
2018-07-01 19:49:44 +0200

    PIK

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from utils import load
+
 FEATURES = [
     u'start_square', u'price', u'mean_sq', u'mean_fl', u'plan_s', u'plan_m', u'plan_l',
     u'vid_0', u'vid_1', u'vid_2', u'month', u'month_cnt', u'Класс объекта',
@@ -17,7 +19,8 @@ FEATURES = [
     u'До промки(км)', u'До парка(км)', u'До парка пешком(км)',
     u'Станций метро от кольца', u'Площадь двора', u'Курс',
     u'Cтавка по ипотеке', u'Вклады до 1 года', u'Вклады от 1 года до 3 лет',
-    u'Вклады свыше 3 лет'
+    u'Вклады свыше 3 лет',
+    u'bulk_cat'
 ]
 
 TEST_FEATURES = [
@@ -39,7 +42,7 @@ TEST_FEATURES = [
     u'Вклады свыше 3 лет',
     # u'year',
     # u'time'
-    u'bulk_cat'
+  u'bulk_cat'
 ]
 
 CATEGORICAL_FEATURES = [
@@ -52,8 +55,8 @@ CATEGORICAL_FEATURES = [
 "Колясочные",
 "Система мусоротведения",
 "Подземная парковка",
-"Двор без машин"
-"bulk_cat"
+"Двор без машин",
+'bulk_cat'
 ]
 
 
@@ -75,17 +78,22 @@ def preprocess(data, categorical_features):
 
     # Transforming categorical features
     for feature_name in categorical_features:
+
+        # For new categorical features
+        if feature_name not in data.columns:
+            continue
+
         data[feature_name] = data[feature_name].str.decode('utf-8').astype('category').cat.codes
 
     # Bulk cat
-
-
-    # Adding date features
-    # data['year'] = data.date1.str[:4].astype(float)
-    # data['time'] = data.year + data.date1.str[5:7].astype(float)/ 12.
+    bulk_mapping = load("bulk_mapping_shared.pkl")
+    last_key = max(bulk_mapping.values()) + 1
+    data['bulk_cat'] = data.bulk_id.apply(lambda x: bulk_mapping.get(x, last_key))
 
     # Column names decoding
     data.columns = [c.decode('utf-8') for c in data.columns]
+
+    # print data.columns
 
     return data
 
